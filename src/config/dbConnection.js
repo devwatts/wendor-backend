2d64d51285562fe070d083546a8202120b3139cc
Dev Watts <devwatts5@gmaiil.com>
2022-07-13 16:29:57 +0530

    prod

--- src/config/dbConnection.js
+++ src/config/dbConnection.js
@@ -1,11 +1,7 @@
 const { Client } = require('pg');
-
-const client = new Client({
-    connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
-})
+const dbEngine = process.env.DB_ENVIRONMENT || 'development'
+const config = require('./dbConfig')[dbEngine];
+const client = new Client(config)
 
 client.connect(err => {
     if (err) {
