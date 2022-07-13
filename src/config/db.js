446d27a846681f2f10c7072dc4577d1ed80763f6
Dev Watts <devwatts5@gmaiil.com>
2022-07-13 15:34:54 +0530

    -

--- src/config/db.js
+++ src/config/db.js
@@ -1,10 +1,10 @@
 const { Client } = require('pg');
 
 const client = new Client({
-  host: 'ec2-3-219-229-143.compute-1.amazonaws.com',
-  port: 5432,
-  user: 'esfdfblhwbrfnf',
-  password: '69f4838696b54f5720a8951e237e86fda8457c9ecc8834eda57409e72c36d7bf',
+    connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 })
 
 client.connect(err => {
